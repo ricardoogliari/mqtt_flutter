d596e224ec795d309a88b38656c2c35800bdd426
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2023-12-20 22:22:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mqtt_flutter
+
+First: I found an interesting article in Medium: Building Real-Time IoT Applications with MQTT in Flutter Web
+https://medium.com/@durgeshparekh381/implement-mqtt-in-flutter-web-1d7d954c9cdf
+
+But, the example in this article use a old version of mqtt_client. Then, a made a simple example using flutter web that 
+receive a value by mqtt and, change the position of the slider on the screen. 
+
+* Command to publish a mqtt message on terminal: mosquitto_pub -h 'test.mosquitto.org' -t 'test/lol' -m '95'
---
+++ lib/main.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_browser_client.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final client = MqttBrowserClient('ws://test.mosquitto.org', '');
+
+  double _sliderValue = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        actions: [
+          TextButton(onPressed: subscription, child: const Text("Subscription")),
+          TextButton(onPressed: connect, child: const Text("Connect"))
+        ],
+      ),
+      body: Center(
+        child: Slider(
+          value: _sliderValue,
+          min: 0,
+          max: 100,
+          label: _sliderValue.round().toString(),
+          onChanged: (value) {
+            setState(() {
+              _sliderValue = value;
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  void subscription() {
+    /// Ok, lets try a subscription
+    print('EXAMPLE::Subscribing to the test/lol topic');
+    const topic = 'test/lol'; // Not a wildcard topic
+    client.subscribe(topic, MqttQos.atMostOnce);
+
+    /// The client has a change notifier object(see the Observable class) which we then listen to to get
+    /// notifications of published updates to each subscribed topic.
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+      final recMess = c![0].payload as MqttPublishMessage;
+      final pt = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+      /// The above may seem a little convoluted for users only interested in the
+      /// payload, some users however may be interested in the received publish message,
+      /// lets not constrain ourselves yet until the package has been in the wild
+      /// for a while.
+      /// The payload is a byte buffer, this will be specific to the topic
+      print('EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+
+      setState(() {
+        _sliderValue = double.parse(pt);
+      });
+    });
+
+    /// If needed you can listen for published messages that have completed the publishing
+    /// handshake which is Qos dependant. Any message received on this stream has completed its
+    /// publishing handshake with the broker.
+    client.published!.listen((MqttPublishMessage message) {
+      print('EXAMPLE::Published notification:: topic is ${message.variableHeader!.topicName}, with Qos ${message.header!.qos}');
+    });
+  }
+
+  void connect() async {
+    /// Set logging on if needed, defaults to off
+    client.logging(on: false);
+
+    /// Set the correct MQTT protocol for mosquito
+    client.setProtocolV311();
+
+    /// If you intend to use a keep alive you must set it here otherwise keep alive will be disabled.
+    client.keepAlivePeriod = 20;
+
+    /// The connection timeout period can be set if needed, the default is 5 seconds.
+    client.connectTimeoutPeriod = 2000; // milliseconds
+
+    /// The ws port for Mosquitto is 8080, for wss it is 8081
+    client.port = 8080;
+
+    /// Add the unsolicited disconnection callback
+    client.onDisconnected = onDisconnected;
+
+    /// Add the successful connection callback
+    client.onConnected = onConnected;
+
+    /// Add a subscribed callback, there is also an unsubscribed callback if you need it.
+    /// You can add these before connection or change them dynamically after connection if
+    /// you wish. There is also an onSubscribeFail callback for failed subscriptions, these
+    /// can fail either because you have tried to subscribe to an invalid topic or the broker
+    /// rejects the subscribe request.
+    client.onSubscribed = onSubscribed;
+
+    /// Set a ping received callback if needed, called whenever a ping response(pong) is received
+    /// from the broker.
+    client.pongCallback = pong;
+
+    /// Set the appropriate websocket headers for your connection/broker.
+    /// Mosquito uses the single default header, other brokers may be fine with the
+    /// default headers.
+    client.websocketProtocols = MqttClientConstants.protocolsSingleDefault;
+
+    /// Create a connection message to use or use the default one. The default one sets the
+    /// client identifier, any supplied username/password and clean session,
+    /// an example of a specific one below.
+    final connMess = MqttConnectMessage()
+        .withClientIdentifier('Mqtt_MyClientUniqueId')
+        .withWillTopic('willtopic') // If you set this you must set a will message
+        .withWillMessage('My Will message')
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atLeastOnce);
+    print('EXAMPLE::Mosquitto client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+    /// never send malformed messages.
+    try {
+      await client.connect();
+    } on Exception catch (e) {
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    }
+
+    /// Check we are connected
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('EXAMPLE::Mosquitto client connected');
+    } else {
+      /// Use status here rather than state if you also want the broker return code.
+      print(
+          'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+      client.disconnect();
+    }
+  }
+
+  // connection succeeded
+  void onConnected() {
+    print('Connected');
+  }
+
+// unconnected
+  void onDisconnected() {
+    print('Disconnected');
+  }
+
+// subscribe to topic succeeded
+  void onSubscribed(String topic) {
+    print('Subscribed topic: $topic');
+  }
+
+// subscribe to topic failed
+  void onSubscribeFail(String topic) {
+    print('Failed to subscribe $topic');
+  }
+
+// unsubscribe succeeded
+  void onUnsubscribed(String topic) {
+    print('Unsubscribed topic: $topic');
+  }
+
+// PING response received
+  void pong() {
+    print('Ping response client callback invoked');
+  }
+}
